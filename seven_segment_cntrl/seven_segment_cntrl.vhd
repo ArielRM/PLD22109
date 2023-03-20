a62a9fd1e7a2b9d0a678ead906c167f4b132d5d8
Ariel <arielmontardo@gmail.com>
2023-03-20 18:30:08 -0300

    arruma a lógica do display de 7 segmentos

--- seven_segment_cntrl/seven_segment_cntrl.vhd
+++ seven_segment_cntrl/seven_segment_cntrl.vhd
@@ -12,31 +12,32 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+-- Decodificador 7-segmentos, a ordem dos bit é `"pgfedcba"`
 entity seven_segment_cntrl is
     port(
         input : in  std_logic_vector(3 downto 0);
         dp    : in  std_logic;
-        segs  : out std_logic_vector(7 downto 0)
+        segs  : out std_logic_vector(7 downto 0) -- `"pgfedcba"`
     );
 end entity seven_segment_cntrl;
 
 architecture RTL of seven_segment_cntrl is
-    constant AC_0 : std_logic_vector(6 downto 0) := "0000001";
-    constant AC_1 : std_logic_vector(6 downto 0) := "1001111";
-    constant AC_2 : std_logic_vector(6 downto 0) := "0010010";
-    constant AC_3 : std_logic_vector(6 downto 0) := "0000110";
-    constant AC_4 : std_logic_vector(6 downto 0) := "1001100";
-    constant AC_5 : std_logic_vector(6 downto 0) := "0100100";
-    constant AC_6 : std_logic_vector(6 downto 0) := "0100000";
-    constant AC_7 : std_logic_vector(6 downto 0) := "0001111";
+    constant AC_0 : std_logic_vector(6 downto 0) := "1000000";
+    constant AC_1 : std_logic_vector(6 downto 0) := "1111001";
+    constant AC_2 : std_logic_vector(6 downto 0) := "0100100";
+    constant AC_3 : std_logic_vector(6 downto 0) := "0110000";
+    constant AC_4 : std_logic_vector(6 downto 0) := "0011001";
+    constant AC_5 : std_logic_vector(6 downto 0) := "0010010";
+    constant AC_6 : std_logic_vector(6 downto 0) := "0000010";
+    constant AC_7 : std_logic_vector(6 downto 0) := "1111000";
     constant AC_8 : std_logic_vector(6 downto 0) := "0000000";
-    constant AC_9 : std_logic_vector(6 downto 0) := "0001100";
+    constant AC_9 : std_logic_vector(6 downto 0) := "0011000";
     constant AC_A : std_logic_vector(6 downto 0) := "0001000";
-    constant AC_B : std_logic_vector(6 downto 0) := "1100000";
-    constant AC_C : std_logic_vector(6 downto 0) := "0110001";
-    constant AC_D : std_logic_vector(6 downto 0) := "1000010";
-    constant AC_E : std_logic_vector(6 downto 0) := "0110000";
-    constant AC_F : std_logic_vector(6 downto 0) := "0111000";
+    constant AC_B : std_logic_vector(6 downto 0) := "0000011";
+    constant AC_C : std_logic_vector(6 downto 0) := "1000110";
+    constant AC_D : std_logic_vector(6 downto 0) := "0100001";
+    constant AC_E : std_logic_vector(6 downto 0) := "0000110";
+    constant AC_F : std_logic_vector(6 downto 0) := "0001110";
 
 begin
     process(input, dp) is
@@ -49,37 +50,37 @@ begin
         -- acionar o `decimal point` em `1` 
         case input is
             when x"0" =>
-                segs <= AC_0 & not dp;
+                segs <= not dp & AC_0;
             when x"1" =>
-                segs <= AC_1 & not dp;
+                segs <= not dp & AC_1;
             when x"2" =>
-                segs <= AC_2 & not dp;
+                segs <= not dp & AC_2;
             when x"3" =>
-                segs <= AC_3 & not dp;
+                segs <= not dp & AC_3;
             when x"4" =>
-                segs <= AC_4 & not dp;
+                segs <= not dp & AC_4;
             when x"5" =>
-                segs <= AC_5 & not dp;
+                segs <= not dp & AC_5;
             when x"6" =>
-                segs <= AC_6 & not dp;
+                segs <= not dp & AC_6;
             when x"7" =>
-                segs <= AC_7 & not dp;
+                segs <= not dp & AC_7;
             when x"8" =>
-                segs <= AC_8 & not dp;
+                segs <= not dp & AC_8;
             when x"9" =>
-                segs <= AC_9 & not dp;
+                segs <= not dp & AC_9;
             when x"A" =>
-                segs <= AC_A & not dp;
+                segs <= not dp & AC_A;
             when x"B" =>
-                segs <= AC_B & not dp;
+                segs <= not dp & AC_B;
             when x"C" =>
-                segs <= AC_C & not dp;
+                segs <= not dp & AC_C;
             when x"D" =>
-                segs <= AC_D & not dp;
+                segs <= not dp & AC_D;
             when x"E" =>
-                segs <= AC_E & not dp;
+                segs <= not dp & AC_E;
             when x"F" =>
-                segs <= AC_F & not dp;
+                segs <= not dp & AC_F;
             when others =>
                 -- Um pouco redundante com o valor padrão, mas pode evitar erros futuros.
                 segs <= x"FF";
